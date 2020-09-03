eea6410ce8f3f1669db19dfea964d70bccc588b9
GianElli99 <gianelli99@hotmail.com>
2020-09-03 09:32:31 -0300

    bug sql connection fixed

--- Lawful.Core/Datos/ConexionDB.cs
+++ Lawful.Core/Datos/ConexionDB.cs
@@ -4,10 +4,6 @@ namespace Lawful.Core.Datos
 {
     public class ConexionDB
     {
-
-        //protected SqlConnection Conexion = new SqlConnection(@"Server=localhost\SQLEXPRESS;Initial Catalog=ModuloDeSeguridad;Integrated Security=SSPI");
-        //protected SqlConnection Conexion = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=ModuloDeSeguridad;Integrated Security=SSPI");
         protected SqlConnection Conexion = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=ModuloDeSeguridad;Integrated Security=SSPI;");
-        // @"Server=(LocalDb)\MSSQLLocalDB;DataBase=ModuloDeSeguridad;Integrated Security=true"
     }
 }
