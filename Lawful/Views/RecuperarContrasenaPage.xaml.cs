5ab313bf4e8cf76432963cb717d746bc039d1044
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2020-09-25 20:51:34 -0300

    us-forgot-password finished

--- Lawful/Views/RecuperarContrasenaPage.xaml.cs
+++ Lawful/Views/RecuperarContrasenaPage.xaml.cs
@@ -3,34 +3,83 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 using Windows.UI.Xaml.Controls;
+using System.Net.Mail;
+using Lawful.Core;
 
 namespace Lawful.Views
 {
-    public sealed partial class RecuperarContrasenaPage : Page, INotifyPropertyChanged
+    public sealed partial class RecuperarContrasenaPage : Page
     {
+        Core.Logica.UsuarioBL usuarioBL;
         public RecuperarContrasenaPage()
         {
             InitializeComponent();
+            usuarioBL = new Core.Logica.UsuarioBL();
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
-        {
-            if (Equals(storage, value))
-            {
-                return;
-            }
-
-            storage = value;
-            OnPropertyChanged(propertyName);
-        }
-
-        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
         private void btnVolver_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(LoginPage));
         }
+
+        private void btnEnviarCorreo_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+            if (String.IsNullOrWhiteSpace(txtUsername.Text) || String.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                showUIContentDialog("Debe completar todos los campos", false);
+                return;
+            }
+            try
+            {
+                var addr = new MailAddress(txtEmail.Text);
+                if (addr.Address != txtEmail.Text)
+                {
+                    showUIContentDialog("El email no es válido", false);
+                    return;
+                }
+            }
+            catch
+            {
+
+                showUIContentDialog("El email no es válido", false);
+                return;
+            }
+            try
+            {
+                Core.Modelo.Usuario usuario = usuarioBL.Consultar(txtUsername.Text, txtEmail.Text);
+                if (usuario == null)
+                {
+                    showUIContentDialog("Los datos ingresados son incorrectos", false);
+                    return;
+                }
+
+                string passDES = Core.Logica.Hasheo.RandomString(6, true);
+                usuario.Password = Core.Logica.Hasheo.GenerarContrasena(passDES);
+                usuarioBL.CambiarContrasena(passDES, usuario.ID, usuario.ID, true);
+                usuarioBL.EnviarEmail(passDES, usuario.Email);
+
+                showUIContentDialog("Revise su correo electrónico para conocer su nueva contraseña",true);
+            }
+            catch (Exception)
+            {
+                showUIContentDialog("Ha ocurrido un error", false);
+            }
+
+            this.Frame.Navigate(typeof(LoginPage));
+        }
+        private async void showUIContentDialog(string err, bool emailSentSuccessfully)
+        {
+            ContentDialog error = new ContentDialog
+            {
+                Title = emailSentSuccessfully ? "Envío completado correctamente" : "Error",
+                Content = err,
+                CloseButtonText = "Ok"
+            };
+
+            await error.ShowAsync();
+        }
+    
     }
 }
