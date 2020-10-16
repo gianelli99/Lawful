3e3260faef7cdd5bf20c26bd10262b85367bc2d0
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2020-10-16 17:44:57 -0300

    Working on strategies, misplaced some file changes

--- Lawful.Core/Datos/Strategies/DoDontStrategy.cs
+++ Lawful.Core/Datos/Strategies/DoDontStrategy.cs
@@ -4,7 +4,7 @@ using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Lawful.Core.Modelo.Iniciativas
 namespace Lawful.Core.Datos.Strategies
 {
     public class DoDontStrategy : IQueryStrategy
@@ -13,12 +13,59 @@ namespace Lawful.Core.Datos.Strategies
 
         public SqlCommand SetInsertCommand(SqlCommand command, Iniciativa iniciativa)
         {
-            throw new NotImplementedException();
+            DoDont dodont = (DoDont)iniciativa;
+            command.CommandText = "INSERT INTO iniciativas " +
+                "(titulo, descripcion, fecha_creacion, icon_name, everyone_can_edit, usuario_id, iniciativa_tipo_id, fecha_evento, lugar, fecha_limite_confirmacion, tema_id)" +
+                " VALUES " +
+                "(@titulo, @descripcion, @fecha_creacion, @icon_name, @everyone_can_edit, @usuario_id, @iniciativa_tipo_id, @fecha_evento, @lugar, @fecha_limite_confirmacion, @tema_id);";
+
+            command.Parameters.AddWithValue("@titulo", dodont.Titulo);
+            command.Parameters.AddWithValue("@descripcion", dodont.Descripcion);
+            command.Parameters.AddWithValue("@fecha_creacion", dodont.FechaCreacion);
+            command.Parameters.AddWithValue("@icon_name", dodont.IconName);
+            command.Parameters.AddWithValue("@everyone_can_edit", dodont.EveryoneCanEdit);
+            command.Parameters.AddWithValue("@usuario_id", dodont.Owner.ID);
+            command.Parameters.AddWithValue("@iniciativa_tipo_id", Tipo);
+
+            command.Parameters.AddWithValue("@fecha_evento", dodont.FechaEvento);
+            command.Parameters.AddWithValue("@lugar", dodont.Lugar);
+            command.Parameters.AddWithValue("@fecha_limite_confirmacion", dodont.FechaLimiteConfirmacion);
+            command.Parameters.AddWithValue("@tema_id", dodont.Tema.ID);
+
+
+            return command;
         }
 
         public SqlCommand SetUpdateCommand(SqlCommand command, Iniciativa iniciativa)
         {
-            throw new NotImplementedException();
+            DoDont dodont = (DoDont)iniciativa;
+            command.CommandText = "UPDATE iniciativas " +
+                "titulo=@titulo, " +
+                "descripcion=@descripcion, " +
+                "fecha_creacion=@fecha_creacion, " +
+                "icon_name=@icon_name, " +
+                "everyone_can_edit=@everyone_can_edit, " +
+                "usuario_id=@usuario_id, " +
+                "iniciativa_tipo_id=@iniciativa_tipo_id, " +
+                "fecha_evento=@fecha_evento, lugar=@lugar, " +
+                "fecha_limite_confirmacion=@fecha_limite_confirmacion, " +
+                "tema_id=@tema_id " +
+                $"WHERE id = {iniciativa.ID};";
+
+            command.Parameters.AddWithValue("@titulo", dodont.Titulo);
+            command.Parameters.AddWithValue("@descripcion", dodont.Descripcion);
+            command.Parameters.AddWithValue("@fecha_creacion", dodont.FechaCreacion);
+            command.Parameters.AddWithValue("@icon_name", dodont.IconName);
+            command.Parameters.AddWithValue("@everyone_can_edit", dodont.EveryoneCanEdit);
+            command.Parameters.AddWithValue("@usuario_id", dodont.Owner.ID);
+            command.Parameters.AddWithValue("@iniciativa_tipo_id", Tipo);
+
+            command.Parameters.AddWithValue("@fecha_evento", dodont.FechaEvento);
+            command.Parameters.AddWithValue("@lugar", dodont.Lugar);
+            command.Parameters.AddWithValue("@fecha_limite_confirmacion", dodont.FechaLimiteConfirmacion);
+            command.Parameters.AddWithValue("@tema_id", dodont.Tema.ID);
+
+            return command;
         }
     }
 }
